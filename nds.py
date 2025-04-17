75086ec2bfdfc9f44a46349f7c948d4eaf2e10a4
Human <usufhodzaevz@gmail.com>
2025-04-17 20:50:53 +0500

    nds.py

--- nds.py
+++ nds.py
@@ -16,7 +16,7 @@ async def calculate(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Пожалуйста, введи корректное число.")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, calculate))
